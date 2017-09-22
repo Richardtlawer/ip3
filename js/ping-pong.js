eb3cd2959a8e63fde59cf0daed1e55af6453b2cb
Richard Teye Lawer <richardlawer@gmail.com>
2017-09-22 16:18:12 +0000

    second drafted prototype

---
+++ js/ping-pong.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+var numbers=[];
+var numberEntered=$(".input").val();
+
+$(".button").click(function(){
+  for(number=1; number<=numberEntered; number++){
+        numbers=number;
+  alert(number);      
+};
+  $(".lable").text(numbers.pop());
+
+});
+
+
+}
+
+
+
+);
