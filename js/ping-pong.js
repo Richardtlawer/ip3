412423cdcd4fd6a3690e21b6ed73880fc4863c23
Richard Teye Lawer <richardlawer@gmail.com>
2017-09-22 17:45:59 +0000

    fouth drafted prototype

--- js/ping-pong.js
+++ js/ping-pong.js
@@ -4,10 +4,10 @@ var numbers=[];
 var numberEntered=parseInt($(".inReceive").val());
 
 $(".buttonPush").click(function(){
-	alert(numberEntered);
+	
   for(var number=1; number<=numberEntered.length; number+=1){
         numbers=number;
-  alert(number);
+  
   $(".showNumbers").text(numbers.pop());
 
 }
